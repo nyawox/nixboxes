10d2959f2fc725a63495ac62cf3504eccd77de48
nyawox <nyawox.git@gmail.com>
2024-04-27 09:57:11 +0900

    add flatpak overrides

--- modules/nixos/services/flatpak.nix
+++ modules/nixos/services/flatpak.nix
@@ -17,6 +17,16 @@ in {
   config = mkIf cfg.enable {
     services.flatpak = {
       enable = true;
+      overrides = {
+        global = {
+          # Force Wayland by default
+          Context.sockets = ["wayland" "!x11" "!fallback-x11"];
+
+          Environment = {
+            XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+          };
+        };
+      };
     };
     environment.persistence."/persist".directories = [
       "/var/lib/flatpak"
