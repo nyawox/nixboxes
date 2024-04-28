947e3eaf60e114c7b094498afaba8e392aa8e30e
nyawox <nyawox.git@gmail.com>
2024-04-28 17:05:57 +0900

    fix screen lock timeout

--- home/desktop/swayidle.nix
+++ home/desktop/swayidle.nix
@@ -14,12 +14,12 @@
     ];
     timeouts = [
       {
-        timeout = 1800;
+        timeout = 300; # 5 minutes
         command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color f5c2e7 --text-color cdd6f4 --key-hl-color fab387 --line-color 00000000 --inside-color 1e1e2e88 --separator-color 00000000 --grace 2 --fade-in 0.2";
       }
       {
-        timeout = 1805;
-        command = "hyprctl dispatch dpms off";
+        timeout = 120; # 2 minutes
+        command = "${pkgs.niri-unstable}/bin/niri msg action power-off-monitors";
         resumeCommand = "${pkgs.niri-unstable}/bin/niri msg action power-off-monitors";
       }
     ];
