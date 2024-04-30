fc76a2871139630a53be350a33bc395c99f16517
nyawox <nyawox.git@gmail.com>
2024-05-01 08:49:36 +0900

    increase screen lock timeout

--- home/desktop/swayidle.nix
+++ home/desktop/swayidle.nix
@@ -14,11 +14,11 @@
     ];
     timeouts = [
       {
-        timeout = 300; # 5 minutes
+        timeout = 600; # 10 minutes
         command = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color f5c2e7 --text-color cdd6f4 --key-hl-color fab387 --line-color 00000000 --inside-color 1e1e2e88 --separator-color 00000000 --grace 2 --fade-in 0.2";
       }
       {
-        timeout = 120; # 2 minutes
+        timeout = 300; # 5 minutes
         command = "${pkgs.niri-unstable}/bin/niri msg action power-off-monitors";
       }
     ];
