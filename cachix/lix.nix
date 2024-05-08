afbfebaf81b530c751385bb4d4c46f83afc48ff3
nyawox <nyawox.git@gmail.com>
2024-05-08 21:21:11 +0900

    upgrade flake inputs

---
+++ cachix/lix.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.lix.systems"
+      ];
+      trusted-public-keys = [
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+      ];
+    };
+  };
+}
