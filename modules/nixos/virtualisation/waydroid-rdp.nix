b53aa46ae6f8c3e1ea765318899a96a2067cdf80
nyawox <nyawox.git@gmail.com>
2024-05-22 03:08:31 +0900

    remove old remnants of guacamole

--- modules/nixos/virtualisation/waydroid-rdp.nix
+++ modules/nixos/virtualisation/waydroid-rdp.nix
@@ -1,3 +1,5 @@
+# sudo waydroid init -s GAPPS -f
+# systemctl --user restart waydroid-rdp
 {
   config,
   lib,
@@ -10,36 +12,6 @@ with lib; let
     sleep 3
     ${pkgs.waydroid}/bin/waydroid show-full-ui
   '';
-  # guacamole-xml = pkgs.writeText "user-mapping.xml" ''
-  #   <?xml version="1.0" encoding="UTF-8"?>
-  #   <user-mapping>
-  #        <authorize
-  #           username="nyaa"
-  #           password="9970626666560a32465d4ce10d28f3233365af833e15eed59884d9477862c379"
-  #           encoding="sha256">
-  #         <connection name="Android RDP">
-  #             <protocol>rdp</protocol>
-  #             <param name="hostname">127.0.0.1</param>
-  #             <param name="port">3150</param>
-  #             <param name="security">any</param>
-  #             <param name="ignore-cert">true</param>
-  #             <param name="color-depth">24</param>
-  #             <param name="force-loseless">true</param>
-  #             <param name="enable-touch">true</param>
-  #         </connection>
-  #         <connection name="Windows">
-  #             <protocol>rdp</protocol>
-  #             <param name="hostname">127.0.0.1</param>
-  #             <param name="port">3389</param>
-  #             <param name="security">any</param>
-  #             <param name="ignore-cert">true</param>
-  #             <param name="color-depth">24</param>
-  #             <param name="force-loseless">true</param>
-  #             <param name="enable-touch">true</param>
-  #         </connection>
-  #       </authorize>
-  #   </user-mapping>
-  # '';
 in {
   options = {
     modules.virtualisation.waydroid = {
@@ -84,27 +56,6 @@ in {
         };
       };
     };
-    # modules.virtualisation.arion.enable = lib.mkForce true;
-    # virtualisation.arion.projects.guacamole.settings = {
-    #   project.name = "guacamole";
-    #   services.guacd.service = {
-    #     name = "guacd";
-    #     image = "docker.io/guacamole/guacd:latest";
-    #     labels."io.containers.autoupdate" = "registry";
-    #   };
-    #   services.guacamole.service = {
-    #     image = "docker.io/guacamole/guacamole:latest";
-    #     links = ["guacd"];
-    #     ports = ["8080:8150"];
-    #     environment = {
-    #       POSTGRESQL_HOSTNAME = "nixpro64.nyaa.nixlap.top";
-    #       POSTGRESQL_PORT = 5432; # Default port
-    #       POSTGRESQL_DATABASE = "guacamole";
-    #       POSTGRESQL_USER = "guacamole";
-    #     };
-    #     labels."io.containers.autoupdate" = "registry";
-    #   };
-    # };
     environment = {
       etc."waydroid-cloud.ini".text = lib.generators.toINI {} {
         autolaunch = {
