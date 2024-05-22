c52e5dc2bef6fcb5f873320d208dd8606a17cc86
nyawox <nyawox.git@gmail.com>
2024-05-23 04:52:55 +0900

    revert to lix from nixpkgs, unable to passtest

--- flake.nix
+++ flake.nix
@@ -4,16 +4,6 @@
     latest.url = "github:nixos/nixpkgs";
     nixpkgs-yuzu.url = "github:nixos/nixpkgs/95002f7";
     # Make sure to follow nixpkgs to unclutter lock file
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
     nix-darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -191,7 +181,7 @@
         };
 
         nixosModules = {
-          common = {...}: {
+          common = {pkgs, ...}: {
             nixpkgs = {
               config = {
                 allowUnfree = true;
@@ -218,7 +208,7 @@
             };
             # Enable flakes
             nix = {
-              # package = pkgs.lix;
+              package = pkgs.lix;
               settings = {
                 experimental-features = ["nix-command" "flakes" "repl-flake"];
                 trusted-users = ["@wheel"];
@@ -235,7 +225,7 @@
                   hostName = "192.168.0.132";
                   sshUser = "root";
                   system = "aarch64-linux";
-                  maxJobs = 8;
+                  maxJobs = 1;
                   speedFactor = 10;
                   supportedFeatures = ["benchmark" "big-parallel" "kvm" "nixos-test"];
                   mandatoryFeatures = [];
@@ -255,7 +245,6 @@
               ./modules/nixos/globalvars.nix
               ./modules/nixos/warnings.nix
               inputs.sops-nix.nixosModules.sops
-              inputs.lix-module.nixosModules.default
             ];
           };
           # NixOS specific configuration
