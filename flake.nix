79a0ef748539f5e0c92994bb6688f1a99f883281
nyawox <nyawox.git@gmail.com>
2024-05-22 14:05:55 +0900

    try lix beta

--- flake.nix
+++ flake.nix
@@ -4,16 +4,16 @@
     latest.url = "github:nixos/nixpkgs";
     nixpkgs-yuzu.url = "github:nixos/nixpkgs/95002f7";
     # Make sure to follow nixpkgs to unclutter lock file
-    # lix = {
-    #   url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-    #   flake = false;
-    # };
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
 
-    # lix-module = {
-    #   url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    #   inputs.lix.follows = "lix";
-    # };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
     nix-darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -242,7 +242,7 @@
               ./modules/nixos/globalvars.nix
               ./modules/nixos/warnings.nix
               inputs.sops-nix.nixosModules.sops
-              # inputs.lix-module.nixosModules.default
+              inputs.lix-module.nixosModules.default
             ];
           };
           # NixOS specific configuration
