5eafdbf51431c42c58f7e9f908af0375bf7507a8
nyawox <nyawox.git@gmail.com>
2024-05-23 04:58:14 +0900

    disable NIXOS_OZONE_WL because of scaling issue

--- home/desktop/cosmic/general.nix
+++ home/desktop/cosmic/general.nix
@@ -7,8 +7,6 @@
 with lib; {
   config = mkIf osConfig.modules.desktop.cosmic.enable {
     home.sessionVariables = {
-      # force wayland
-      NIXOS_OZONE_WL = "1";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1"; # Disables window decorations on Qt applications
       GTK_THEME = config.gtk.theme.name;
     };
