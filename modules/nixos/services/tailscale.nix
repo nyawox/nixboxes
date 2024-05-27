ff677e94192465b3cbd2f4d48309d1fab508d6c4
nyawox <nyawox.git@gmail.com>
2024-05-27 19:49:52 +0900

    add encrypted tailscale preauthkey

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -9,7 +9,6 @@ with lib;
 let
   cfg = config.modules.services.tailscale;
   loginserver = "https://headscale.nixlap.top";
-  authkey = "18d2a845a057f5b3ee0b3b68df8174b7e8c38ce139dac306";
 in
 {
   options = {
@@ -33,6 +32,10 @@ in
     environment.persistence."/persist".directories = mkIf config.modules.sysconf.impermanence.enable [
       "/var/lib/tailscale"
     ];
+    sops.secrets."tailscale_preauthkey" = {
+      sopsFile = ../../../secrets/tailscale_preauthkey.psk;
+      format = "binary";
+    };
 
     # create a oneshot job to authenticate to Tailscale
     systemd.services.tailscale-autoconnect = {
@@ -55,7 +58,7 @@ in
       # have the job run this shell script
       script = with pkgs; ''
         # wait for tailscaled to settle
-        sleep 2
+        sleep 5
 
         # check if we are already authenticated to tailscale
         status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
@@ -64,7 +67,7 @@ in
         fi
 
         # otherwise authenticate with tailscale
-        ${tailscale}/bin/tailscale up --login-server ${loginserver} --auth-key ${authkey}
+        ${tailscale}/bin/tailscale up --login-server ${loginserver} --auth-key $(${coreutils}/bin/cat /run/secrets/tailscale_preauthkey)
         # don't forget to replace auth key each time
       '';
     };
