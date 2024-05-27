e1ef533f67a6a54ee46e667f520203f72b7040f6
nyawox <nyawox.git@gmail.com>
2024-05-27 21:25:04 +0900

    pipewire: surround values with quotes

--- modules/nixos/desktop/pipewire.nix
+++ modules/nixos/desktop/pipewire.nix
@@ -30,27 +30,27 @@ in
       #jack.enable = true;
       extraConfig.pipewire = {
         "92-low-latency.conf" = {
-          context.properties = {
-            default.clock.rate = 48000;
-            default.clock.quantum = 32;
-            default.clock.min-quantum = 32;
-            default.clock.max-quantum = 32;
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.quantum" = 32;
+            "default.clock.min-quantum" = 32;
+            "default.clock.max-quantum" = 32;
           };
-          context.modules = [
+          "context.modules" = [
             {
               name = "libpipewire-module-protocol-pulse";
               args = {
-                pulse.min.req = "32/48000";
-                pulse.default.req = "32/48000";
-                pulse.max.req = "32/48000";
-                pulse.min.quantum = "32/48000";
-                pulse.max.quantum = "32/48000";
+                "pulse.min.req" = "32/48000";
+                "pulse.default.req" = "32/48000";
+                "pulse.max.req" = "32/48000";
+                "pulse.min.quantum" = "32/48000";
+                "pulse.max.quantum" = "32/48000";
               };
             }
           ];
-          stream.properties = {
-            node.latency = "32/48000";
-            resample.quality = 1;
+          "stream.properties" = {
+            "node.latency" = "32/48000";
+            "resample.quality" = 1;
           };
         };
       };
