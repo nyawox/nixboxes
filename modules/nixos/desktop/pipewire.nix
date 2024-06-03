d8717192d3f9b9b6531c28e6fd56b2b4c4e10b29
nyawox <nyawox.git@gmail.com>
2024-06-04 00:59:17 +0900

    I hope this fixes audio crackling

--- modules/nixos/desktop/pipewire.nix
+++ modules/nixos/desktop/pipewire.nix
@@ -26,34 +26,6 @@ in
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-      extraConfig.pipewire = {
-        "92-low-latency.conf" = {
-          "context.properties" = {
-            "default.clock.rate" = 48000;
-            "default.clock.quantum" = 32;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 32;
-          };
-          "context.modules" = [
-            {
-              name = "libpipewire-module-protocol-pulse";
-              args = {
-                "pulse.min.req" = "32/48000";
-                "pulse.default.req" = "32/48000";
-                "pulse.max.req" = "32/48000";
-                "pulse.min.quantum" = "32/48000";
-                "pulse.max.quantum" = "32/48000";
-              };
-            }
-          ];
-          "stream.properties" = {
-            "node.latency" = "32/48000";
-            "resample.quality" = 1;
-          };
-        };
-      };
       wireplumber = {
         enable = true;
         configPackages = [
