13deefa9a4aa2e972be4cb65a5d9798d872bc8f2
nyawox <nyawox.git@gmail.com>
2024-06-15 21:23:35 +0900

    fix open-webui using container

--- modules/nixos/services/open-webui.nix
+++ modules/nixos/services/open-webui.nix
@@ -5,6 +5,7 @@
 }:
 with lib; let
   cfg = config.modules.services.open-webui;
+  ipSubnet = "172.27.0.0/16";
 in {
   options = {
     modules.services.open-webui = {
@@ -12,25 +13,56 @@ in {
         type = types.bool;
         default = false;
       };
+      port = mkOption {
+        type = types.int;
+        default = 11454;
+      };
     };
   };
   config = mkIf cfg.enable {
-    services.open-webui = {
-      enable = true;
-      host = "0.0.0.0";
-      port = 11454;
-      openFirewall = false;
-      environment = {
-        OLLAMA_API_BASE_URL = "http://lolcathost.nyaa.nixlap.top:11451";
-        ANONYMIZED_TELEMETRY = "False";
-        DO_NOT_TRACK = "True";
-        SCARF_NO_ANALYTICS = "True";
+    virtualisation.arion.projects.open-webui.settings = {
+      project.name = "open-webui";
+      networks = {
+        default = {
+          name = "open-webui";
+          ipam = {
+            config = [{subnet = ipSubnet;}];
+          };
+        };
+      };
+
+      services.open-webui = {
+        service = {
+          image = "ghcr.io/open-webui/open-webui:main";
+          container_name = "open-webui";
+          environment = {
+            OLLAMA_API_BASE_URL = "http://lolcathost.nyaa.nixlap.top:11451";
+            ANONYMIZED_TELEMETRY = "False";
+            DO_NOT_TRACK = "True";
+            SCARF_NO_ANALYTICS = "True";
+          };
+          volumes = ["/var/lib/open-webui:/app/backend/data"];
+          restart = "unless-stopped";
+          ports = [
+            "${builtins.toString cfg.port}:8080"
+          ];
+          labels."io.containers.autoupdate" = "registry";
+        };
       };
     };
-    environment.persistence."/persist".directories = mkIf config.modules.sysconf.impermanence.enable (lib.singleton {
-      directory = "/var/lib/open-webui";
-      user = "open-webui";
-      group = "open-webui";
-    });
+    systemd.services.arion-open-webui = {
+      wants = ["network-online.target"];
+      after = ["network-online.target"];
+    };
+    networking = {
+      nftables.enable = lib.mkForce false;
+      firewall.extraCommands = ''
+        iptables -A INPUT -p tcp --destination-port 53 -s ${ipSubnet} -j ACCEPT
+        iptables -A INPUT -p udp --destination-port 53 -s ${ipSubnet} -j ACCEPT
+      '';
+    };
+    environment.persistence."/persist".directories = mkIf config.modules.sysconf.impermanence.enable [
+      "/var/lib/open-webui"
+    ];
   };
 }
