3a58dfed665cf2b1888d653d54c6294ebad3cf39
nyawox <nyawox.git@gmail.com>
2024-06-19 18:49:12 +0900

    firefox: add jiffy-reader

--- home/desktop/schizofox.nix
+++ home/desktop/schizofox.nix
@@ -101,6 +101,7 @@ in {
           "{a218c3db-51ef-4170-804b-eb053fc9a2cd}".install_url = "${exturl}/qr-code-address-bar/latest.xpi";
           "searxng-favicons@micahmo.com".install_url = "${exturl}/searxng-favicons/latest.xpi";
           "{46e5cbb1-2128-4001-9397-a941b8017863}".install_url = "${exturl}/nook/latest.xpi";
+          "{93f81583-1fd4-45cc-bff4-abba952167bb}".install_url = "${exturl}/jiffy-reader/latest.xpi";
           # "userchrome-toggle@joolee.nl".install_url = "${exturl}/userchrome-toggle/latest.xpi";
           # "ATBC@EasonWong".install_url = "${exturl}/adaptive-tab-bar-colour/latest.xpi";
         };
