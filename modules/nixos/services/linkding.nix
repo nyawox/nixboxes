edf1c90a7024a85a065fd098ea3bf0940f973b1b
nyawox <nyawox.git@gmail.com>
2024-06-27 18:09:21 +0900

    add morphic, but disable it

--- modules/nixos/services/linkding.nix
+++ modules/nixos/services/linkding.nix
@@ -5,6 +5,7 @@
 }:
 with lib; let
   cfg = config.modules.services.linkding;
+  ipSubnet = "172.32.0.0/16";
 in {
   options = {
     modules.services.linkding = {
@@ -22,6 +23,14 @@ in {
     modules.virtualisation.arion.enable = lib.mkForce true;
     virtualisation.arion.projects.linkding.settings = {
       project.name = "linkding";
+      networks = {
+        default = {
+          name = "linkding";
+          ipam = {
+            config = [{subnet = ipSubnet;}];
+          };
+        };
+      };
       services.linkding.service = {
         image = "docker.io/sissbruecker/linkding:latest";
         volumes = ["/var/lib/linkding:/etc/linkding/data"];
@@ -32,6 +41,13 @@ in {
         };
         labels."io.containers.autoupdate" = "registry";
       };
+    };
+    networking = {
+      nftables.enable = lib.mkForce false;
+      firewall.extraCommands = ''
+        iptables -A INPUT -p tcp --destination-port 53 -s ${ipSubnet} -j ACCEPT
+        iptables -A INPUT -p udp --destination-port 53 -s ${ipSubnet} -j ACCEPT
+      '';
     };
     environment.persistence."/persist".directories = mkIf config.modules.sysconf.impermanence.enable [
       "/var/lib/linkding"
---
+++ modules/nixos/services/morphic.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.modules.services.morphic;
+  ollama-url = "http://nixpro64.nyaa.nixlap.top:11451";
+  ollama-model = "mistral:7b";
+  ollama-sub-model = "phi3";
+in {
+  options = {
+    modules.services.morphic = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+      port = mkOption {
+        type = types.int;
+        default = 3150;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    sops.secrets.morphic = {
+      sopsFile = ../../../secrets/morphic.env;
+      format = "dotenv";
+    };
+    modules.virtualisation.arion.enable = lib.mkForce true;
+    virtualisation.arion.projects.morphic.settings = {
+      project.name = "morphic";
+      services.morphic.service = {
+        image = "shenlw/morphic:latest";
+        ports = ["3000:3000"];
+        network_mode = "host";
+        environment = {
+          OLLAMA_BASE_URL = ollama-url;
+          OLLAMA_MODEL = ollama-model;
+          OLLAMA_SUB_MODEL = ollama-sub-model;
+        };
+        env_file = [
+          # required for tavily, upstash redis secrets. currently no support for local engine and redis
+          config.sops.secrets.morphic.path
+        ];
+        restart = "unless-stopped";
+      };
+    };
+    systemd.services.arion-morphic = {
+      wants = ["network-online.target"];
+      after = ["network-online.target"];
+    };
+  };
+}
