f27a7a93b3ac6fae839f45db9c296d09914274f1
nyawox <nyawox.git@gmail.com>
2024-07-05 21:07:09 +0900

    enable udisks2 and force btrfs compression

---
+++ modules/nixos/services/udisks2.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.modules.services.udisks2;
+in {
+  options = {
+    modules.services.udisks2 = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    services.udisks2 = {
+      enable = true;
+      settings = {
+        "udisks2.conf" = {
+          defaults = {
+            # no need to default to lower compression levels unless it's a fast SSD
+            btrfs_defaults = "compress-force=zstd:3";
+          };
+        };
+      };
+    };
+  };
+}
