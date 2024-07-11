b7c47932bfd132d594603bdcb8041a111298437e
nyawox <nyawox.git@gmail.com>
2024-07-11 21:50:31 +0900

    remove old hardware.opengl reference

--- hosts/nagisa/default.nix
+++ hosts/nagisa/default.nix
@@ -54,7 +54,7 @@
   boot.initrd.kernelModules = ["i915"];
 
   environment.variables = {
-    VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+    VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "va_gl");
   };
 
   hardware.graphics.extraPackages = with pkgs; [
