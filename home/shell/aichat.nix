2c93b1ee1291d1c032dbce8d7a0c867a42f289c9
nyawox <nyawox.git@gmail.com>
2024-07-11 15:58:59 +0900

    add aichat, I will add shell integration later on

---
+++ home/shell/aichat.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.modules.shell.aichat;
+in {
+  options = {
+    modules.shell.aichat = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages = [pkgs.aichat];
+    xdg.configFile."aichat/config.yaml".text =
+      /*
+      yaml
+      */
+      ''
+        model: ollama
+        clients:
+        - type: ollama
+          api_base: http://nixpro64.nyaa.nixlap.top:11451
+          api_auth: null
+          models:
+          - name: gemma2:9b-instruct-q5_K_M
+            max_input_tokens: 8192
+      '';
+  };
+}
