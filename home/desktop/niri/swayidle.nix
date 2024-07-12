7f1a4f8eb436b20dc3537a4ef3996cb2da0ea048
nyawox <nyawox.git@gmail.com>
2024-07-13 01:52:23 +0900

    adjust swayidle timeout

--- home/desktop/niri/swayidle.nix
+++ home/desktop/niri/swayidle.nix
@@ -20,11 +20,11 @@ in {
       enable = true;
       timeouts = [
         {
-          timeout = 120; # 2 Minutes
+          timeout = 60 * 5;
           command = "${getExe pkgs.swaylock-effects} --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color f5c2e7 --text-color cdd6f4 --key-hl-color fab387 --line-color 00000000 --inside-color 1e1e2e88 --separator-color 00000000 --grace 2 --fade-in 0.2";
         }
         {
-          timeout = 300; # 5 Minutes
+          timeout = 60 * 10;
           command = "${pkgs.niri-unstable}/bin/niri msg action power-off-monitors";
         }
       ];
