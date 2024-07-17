5804b80b5812712a8ea5dcff351a063595a61885
nyawox <nyawox.git@gmail.com>
2024-07-18 00:05:24 +0900

    add open-webui env vars

--- modules/nixos/services/open-webui.nix
+++ modules/nixos/services/open-webui.nix
@@ -26,6 +26,14 @@ in {
       environment = {
         ENV = "prod";
         OLLAMA_BASE_URL = "http://localpost.nyaa.nixlap.top:11451";
+        OPENAI_API_BASE_URL = "https://api.groq.com/openai/v1"; # Add API key manually
+        ENABLE_RAG_WEB_SEARCH = "True";
+        RAG_WEB_SEARCH_ENGINE = "searxng";
+        SEARXNG_QUERY_URL = "http://localghost.nyaa.nixlap.top:8420/search";
+        RAG_WEB_SEARCH_RESULT_COUNT = "5";
+        AUDIO_STT_ENGINE = "openai";
+        AUDIO_STT_OPENAI_API_BASE_URL = "https://api.groq.com/openai/v1"; # Add API key manually
+        AUDIO_STT_MODEL = "whisper-large-v3";
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
