7cad3339e91015c9599c43c0e45fa00256523c69
nyawox <nyawox.git@gmail.com>
2024-07-23 18:23:14 +0900

    this works

--- modules/nixos/services/windows.nix
+++ modules/nixos/services/windows.nix
@@ -33,8 +33,8 @@ in {
         image = "dockurr/windows:3.06";
         environment = {
           # iPhone
-          ARGUMENTS = "-device usb-host,vendorid=0x05ac,productid=0x12a8";
-          # ARGUMENTS = "-device usb-host,hostbus=1,hostport=4";
+          # ARGUMENTS = "-device usb-host,vendorid=0x05ac,productid=0x12a8 -device usb-host,vendorid=0x05ac,productid=0x1281";
+          ARGUMENTS = "-device usb-host,hostbus=1,hostport=4";
           VERSION = "win11";
           RAM_SIZE = "8G";
           CPU_CORES = "12";
