d2555898dc1b5bd30dc15752f67f217f856bf675
nyawox <nyawox.git@gmail.com>
2024-07-28 13:24:03 +0900

    refactor hass config

--- modules/nixos/services/home-assistant.nix
+++ modules/nixos/services/home-assistant.nix
@@ -8,7 +8,41 @@
 with lib; let
   cfg = config.modules.services.home-assistant;
   proxyIP = "100.64.0.1";
-  catppuccin = inputs.catpuccin-home-assistant.outPath + "/themes";
+  catppuccin = "${inputs.catpuccin-home-assistant.outPath}/themes";
+
+  mkIosAction = {
+    name,
+    label,
+    icon,
+    show_in_carplay ? false,
+    show_in_watch ? true,
+    sound ? null,
+  }: {
+    inherit name;
+    label = {text = label;};
+    icon = {
+      inherit icon;
+      color = "#ffffff";
+    };
+    inherit show_in_carplay show_in_watch;
+    data = optionalAttrs (sound != null) {sound = sound;};
+  };
+
+  mkIosAutomation = {
+    name,
+    action,
+    condition ? [],
+  }: {
+    alias = "${name} Automation";
+    trigger = [
+      {
+        platform = "event";
+        event_type = "ios.action_fired";
+        event_data = {actionName = name;};
+      }
+    ];
+    inherit condition action;
+  };
 in {
   options = {
     modules.services.home-assistant = {
@@ -118,7 +152,6 @@ in {
             installPhase = ''
               runHook preInstall
               mkdir $out
-              ls -R
               install -m0644 ./deps/kiosk-mode/dist/kiosk-mode.js $out
 
               runHook postInstall
@@ -139,9 +172,7 @@ in {
         recorder.db_url = "postgresql:///hass";
         http = {
           use_x_forwarded_for = true;
-          trusted_proxies = [
-            proxyIP
-          ];
+          trusted_proxies = [proxyIP];
         };
         bluetooth = {};
         frontend = {
@@ -155,7 +186,6 @@ in {
           device_code = 1129;
           controller_data = "remote.rm_mini";
         };
-        # used by kiosk-mode to toggle header visibility on mobile
         input_boolean = {
           hide_header = {
             name = "Hide Header on Mobile";
@@ -164,108 +194,61 @@ in {
           };
         };
         ios.actions = [
-          {
+          (mkIosAction {
             name = "Lock Front Door";
-            label.text = "Lock front door";
-            icon = {
-              icon = "door_closed_lock";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
-          {
+            label = "Lock front door";
+            icon = "door_closed_lock";
+          })
+          (mkIosAction {
             name = "Unlock Front Door";
-            label.text = "Unlock front door";
-            icon = {
-              icon = "door_open";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
-          {
+            label = "Unlock front door";
+            icon = "door_open";
+            sound = "US-EN-Alexa-Front-Door-Unlocked.wav";
+          })
+          (mkIosAction {
             name = "Meow: Nightlight";
-            label.text = "Night";
-            icon = {
-              icon = "weather_night";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
-          {
+            label = "Night";
+            icon = "weather_night";
+          })
+          (mkIosAction {
             name = "Meow: Daylight";
-            label.text = "Day";
-            icon = {
-              icon = "lightbulb_on";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
-          {
+            label = "Day";
+            icon = "lightbulb_on";
+          })
+          (mkIosAction {
             name = "Meow: Turn Off Light";
-            label.text = "Off";
-            icon = {
-              icon = "lightbulb_off";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
-          {
+            label = "Off";
+            icon = "lightbulb_off";
+          })
+          (mkIosAction {
             name = "Meow: 60 sec timer";
-            label.text = "Turn off after 60 sec";
-            icon = {
-              icon = "timer_settings";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
-          {
+            label = "Turn off after 60 sec";
+            icon = "timer_settings";
+          })
+          (mkIosAction {
             name = "Meow AC: Instant Cooling";
-            label.text = "Instant Cooling";
-            icon = {
-              icon = "snowflake";
-              color = "#ffffff";
-            };
-            show_in_carplay = false;
-            show_in_watch = true;
-          }
+            label = "Instant Cooling";
+            icon = "snowflake";
+          })
         ];
         "automation manual" = [
           {
             alias = "Automatically send signal when light select state changes";
-            mode = "single";
             trigger = [
               {
                 platform = "state";
-                entity_id = ["select.signals_light"];
+                entity_id = "select.signals_light";
               }
             ];
-            condition = [];
             action = [
               {
                 service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
+                target = {entity_id = "button.send_signal_light";};
               }
             ];
           }
-          {
-            alias = "Lock Front Door";
-            trigger = {
-              platform = "event";
-              event_type = "ios.action_fired";
-              event_data = {
-                actionName = "Lock Front Door";
-              };
-            };
+          (mkIosAutomation {
+            name = "Lock Front Door";
             action = [
               {
                 device_id = "d1bc02d79eb3af192fdb98799e160ddb";
@@ -280,16 +263,9 @@ in {
                 message = "Front door has been locked";
               }
             ];
-          }
-          {
-            alias = "Unlock Front Door";
-            trigger = {
-              platform = "event";
-              event_type = "ios.action_fired";
-              event_data = {
-                actionName = "Unlock Front Door";
-              };
-            };
+          })
+          (mkIosAutomation {
+            name = "Unlock Front Door";
             action = [
               {
                 device_id = "d1bc02d79eb3af192fdb98799e160ddb";
@@ -302,262 +278,86 @@ in {
                 domain = "mobile_app";
                 type = "notify";
                 message = "Front door has been unlocked";
-                data = {
-                  sound = "US-EN-Alexa-Front-Door-Unlocked.wav";
-                };
               }
             ];
-          }
-          {
-            alias = "Nightlight";
-            trigger = [
-              {
-                event_type = "ios.action_fired";
-                event_data = {
-                  actionName = "Meow: Nightlight";
-                };
-                platform = "event";
-              }
-            ];
+          })
+          (mkIosAutomation {
+            name = "Meow: Nightlight";
             action = [
               {
                 service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "1. On";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
-                enabled = true;
+                data = {option = "1. On";};
+                target = {entity_id = "select.signals_light";};
               }
               {
                 service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
-                enabled = true;
+                target = {entity_id = "button.send_signal_light";};
               }
               {
                 service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "8. Night";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
+                data = {option = "8. Night";};
+                target = {entity_id = "select.signals_light";};
               }
               {
                 service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
+                target = {entity_id = "button.send_signal_light";};
               }
             ];
-          }
-          {
-            alias = "Daylight";
-            trigger = [
-              {
-                event_data = {
-                  actionName = "Meow: Daylight";
-                };
-                event_type = "ios.action_fired";
-                platform = "event";
-              }
-            ];
+          })
+          (mkIosAutomation {
+            name = "Meow: Daylight";
             action = [
               {
                 service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "1. On";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
-                enabled = true;
+                data = {option = "1. On";};
+                target = {entity_id = "select.signals_light";};
               }
               {
                 service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
-                enabled = true;
+                target = {entity_id = "button.send_signal_light";};
               }
               {
                 service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "6. Daylight";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
+                data = {option = "6. Daylight";};
+                target = {entity_id = "select.signals_light";};
               }
               {
                 service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
+                target = {entity_id = "button.send_signal_light";};
               }
               {
                 service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "5. Max";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
+                data = {option = "5. Max";};
+                target = {entity_id = "select.signals_light";};
               }
               {
                 service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
+                target = {entity_id = "button.send_signal_light";};
               }
             ];
-          }
-          {
-            alias = "Turn off light";
-            trigger = [
-              {
-                event_data = {
-                  actionName = "Meow: Turn Off Light";
-                };
-                event_type = "ios.action_fired";
-                platform = "event";
-              }
-            ];
+          })
+          (mkIosAutomation {
+            name = "Meow: Turn Off Light";
             action = [
               {
                 service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "2. Off";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
+                data = {option = "2. Off";};
+                target = {entity_id = "select.signals_light";};
               }
             ];
-          }
-          {
-            alias = "Turn off light after 60 seconds";
-            trigger = [
-              {
-                event_type = "ios.action_fired";
-                event_data = {
-                  actionName = "Meow: 60 sec timer";
-                };
-                platform = "event";
-              }
-            ];
-            action = [
-              {
-                service = "select.select_option";
-                metadata = {};
-                data = {
-                  option = "10. 60Min Auto-Off";
-                };
-                target = {
-                  entity_id = "select.signals_light";
-                };
-              }
-              {
-                service = "button.press";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "button.send_signal_light";
-                };
-              }
-            ];
-          }
-          {
-            alias = "Meow AC: Instant Cool";
-            trigger = [
-              {
-                event_data = {
-                  actionName = "Meow AC: Instant Cooling";
-                };
-                event_type = "ios.action_fired";
-                platform = "event";
-              }
-            ];
-            action = [
-              {
-                service = "climate.turn_on";
-                metadata = {};
-                data = {};
-                target = {
-                  entity_id = "climate.ac_remo_mini";
-                };
-              }
-              {
-                service = "climate.set_temperature";
-                metadata = {};
-                data = {
-                  temperature = 16;
-                  hvac_mode = "cool";
-                };
-                target = {
-                  entity_id = "climate.ac_remo_mini";
-                };
-              }
-              {
-                service = "climate.set_swing_mode";
-                metadata = {};
-                data = {
-                  swing_mode = "⥮5";
-                };
-                target = {
-                  entity_id = "climate.ac_remo_mini";
-                };
-              }
-              {
-                service = "climate.set_fan_mode";
-                metadata = {};
-                data = {
-                  fan_mode = "4";
-                };
-                target = {
-                  entity_id = "climate.ac_remo_mini";
-                };
-              }
-            ];
-          }
+          })
         ];
         "automation ui" = "!include automations.yaml";
         "scene ui" = "!include scenes.yaml";
       };
     };
-    # multi-line yaml values
-    # sops.secrets."home-assistant-secrets.yaml" = {
-    #   sopsFile = ../../../secrets/home-assistant-secrets.yaml;
-    #   owner = "hass";
-    #   path = "/var/lib/hass/secrets.yaml";
-    #   restartUnits = ["home-assistant.service"];
-    # };
-
     # prevent home-assistant fail to load when UI automations aren't defined yet
     systemd.tmpfiles.rules = [
       "f ${config.services.home-assistant.configDir}/automations.yaml 0755 hass hass"
       "f ${config.services.home-assistant.configDir}/scenes.yaml 0755 hass hass"
       "C ${config.services.home-assistant.configDir}/themes 0755 hass hass - ${catppuccin}"
     ];
+
     environment.persistence."/persist".directories = mkIf config.modules.sysconf.impermanence.enable (singleton {
       directory = "/var/lib/hass";
       user = "hass";
