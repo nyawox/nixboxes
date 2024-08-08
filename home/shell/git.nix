ff6a27101321b4b53c574c950fd4ba53d75f8065
nyawox <nyawox.git@gmail.com>
2024-08-08 18:19:22 +0900

    git: add libsecret and set defaultBranch to main

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 with lib; let
@@ -17,8 +18,13 @@ in {
   config = mkIf cfg.enable {
     programs.git = {
       enable = true;
+      package = pkgs.gitFull;
 
-      extraConfig.pull.rebase = false;
+      extraConfig = {
+        pull.rebase = false;
+        credential.helper = "libsecret"; # required for obsidian-git plugin
+        init.defaultBranch = "main";
+      };
 
       userEmail = "nyawox.git@gmail.com";
       userName = "nyawox";
