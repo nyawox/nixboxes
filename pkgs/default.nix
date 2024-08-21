37c3cb8a0a5a7a91a10a5151ad3b760328141a78
nyawox <nyawox.git@gmail.com>
2024-08-21 13:46:32 +0900

    format

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
-final: prev: {
+final: _prev: {
   catppuccin-fcitx5 = final.callPackage (import ./catppuccin-fcitx5.nix) {};
   lsp-ai = final.callPackage (import ./lsp-ai.nix) {};
   fast-font = final.callPackage (import ./fast-font.nix) {};
