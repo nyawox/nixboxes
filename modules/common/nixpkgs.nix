006e0f454e42a7e60c1cc03fb84a9f334a011331
nyawox <nyawox.git@gmail.com>
2024-10-26 01:27:00 +0900

    calibre 7.20.0 has reached unstable

--- modules/common/nixpkgs.nix
+++ modules/common/nixpkgs.nix
@@ -9,9 +9,6 @@
           inherit (prev) deploy-rs;
           inherit ((inputs.deploy-rs.overlay final prev).deploy-rs) lib;
         };
-        calibre = inputs.latest.legacyPackages.${prev.system}.calibre.overrideAttrs (_oldAttrs: {
-          doInstallCheck = false; # fails on my aarch64 builder
-        }); # until 7.20.0 lands unstable
       })
       (final: prev: import ../../pkgs final prev)
     ];
